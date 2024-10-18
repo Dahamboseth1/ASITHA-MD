25c7b11809fdab6685339ce3d5eb6e1a5881d1de
Dahamboseth1 <164750096+Dahamboseth1@users.noreply.github.com>
2024-10-18 16:06:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=urYE3ZBD#F4z50cKZ0uMyzwsSJa43f6pf5dfyRjoOdkbsEujGF3w",
-MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
+MONGODB: process.env.MONGODB || "mongodb+srv://bosethwanapura:<db_password>@cluster0.3iulz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",//enter mongo db url
 };
